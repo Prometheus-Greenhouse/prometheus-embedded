d22288cb0e74077e5cf8119bc9883c980db27bad
ngontran123 <huynhkiengquan@gmail.com>
2022-08-17 21:24:13 +0700

    Update

--- Arduino_Uno.ino
+++ Arduino_Uno.ino
@@ -1,13 +1,140 @@
 #include<SoftwareSerial.h>
-SoftwareSerial uno(0,1);
+#include<Wire.h>
+#include<LiquidCrystal_I2C.h>
+#include<DHT.h>
+#define DHTPIN 4
+DHT dht(DHTPIN,DHT11);
+LiquidCrystal_I2C lcd(0X27,16,2);
+
+#define WaterSensor 2
+#define SensorIn A0
+#define LED 8
+#define R 13
+#define G 12
+#define B 11
+char c;
+String dataIn;
+String res;
+int light=0;
+
+
 void setup() {
   Serial.begin(57600);
-  uno.begin(9600);
+  Serial3.begin(57600);
+  pinMode(WaterSensor,OUTPUT);
+  pinMode(LED,OUTPUT);
+  pinMode(R,OUTPUT);
+  pinMode(G,OUTPUT);
+  pinMode(B,OUTPUT);
+  lcd.init();
+  lcd.backlight();
+  dht.begin();
 }
 
+int waterLevel()
+{
+  digitalWrite(WaterSensor,HIGH);
+  delay(20);
+  int value=analogRead(SensorIn);
+  digitalWrite(WaterSensor,LOW);
+  return value;
+  }
+
+  
+void handleTopic()
+  {
+    int water=waterLevel();
+    lcd.setCursor(2,0);
+    lcd.print("Water Level:");
+    lcd.setCursor(2,1);
+    lcd.print(water);
+    res="";
+    res+=String(water)+"\n";
+    Serial3.println(res);
+  }
+
+  
+  void DHT11handle()
+  {
+    float h=dht.readHumidity();
+    float t=dht.readTemperature();
+    Serial.print("Humidity:");
+    Serial.println(h);
+    Serial.print("Temperature:");
+    Serial.println(t);
+    res="";
+    res+=String(h)+" "+String(t)+" \n";
+  }
+
+
+void RGB(int r,int g,int b)
+{
+  analogWrite(R,r);
+  analogWrite(G,g);
+  analogWrite(B,b);
+}
+  
+  void lightHandle()
+  {
+   light=analogRead(A1);
+   if(light<100)
+   {
+    res+=("Very bright");
+    RGB(255,0,0);
+   }
+   else if(light<200)
+   {
+    res+=("Bright");
+    RGB(0,0,255);
+   }
+   else if(light<500)
+   {
+    res+=("Light");
+    RGB(60,179,113);
+   }
+   else if(light<800)
+   {
+    res+=("Dim");
+    RGB(255,165,0);
+   }
+   else
+   {
+    res+=("Dark");
+    RGB(106,90,205);
+   }
+   res+="\n";
+   Serial.println(res);
+  }
+
+  
 void loop() {
-uno.println("Hello friend");
-String t=uno.readStringUntil('\r');
-Serial.println(t);
-delay(1000);
+while(Serial3.available()>0)
+{ 
+  c=Serial3.read();
+  if(c=='\n')
+  {
+    break;
+  }
+  else
+  {
+    dataIn+=c;
+  }
+}
+if(c=='\n')
+{
+
+if(dataIn=="sensor/1")
+{
+  lightHandle();
+  delay(2000);
+}
+else if(dataIn=="sensor/2")
+{
+  handleTopic();
+  delay(2000);
+}
+   c=0;
+   dataIn="";
+}
+RGB(0,0,255);
 }
--- Nodemcu/Nodemcu.ino
+++ Nodemcu/Nodemcu.ino
@@ -1,13 +1,174 @@
-#include<SoftwareSerial.h>
-SoftwareSerial uno(1,2);
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <iostream>
+#include <algorithm>
+const char* ssid = "long 2.4G";
+const char* password = "Ld201199";
+const char* mqtt_server = "192.168.1.3";
+WiFiClient espClient;
+PubSubClient client(espClient);
+unsigned long lastMsg = 0;
+#define MSG_BUFFER_SIZE  (50)
+#define EspSerial Serial3
+char msg[MSG_BUFFER_SIZE];
+int value = 0;
+#define WaterSensor 4
+const int ledGPIO5=5;
+#define sensorIn A0
+const char *Topic="sensor/0";
+String message;
+String _topic;
+String mess_setup="fuck off.";
+const char *Toopic="available";
+const char *sensor1="sensor/1";
+int counter=0;
+int counter1=0;
+String split_str[5];
+char c;
+String dataIn;
 void setup() {
- Serial.begin(115200);
- uno.begin(9600);
+  pinMode(ledGPIO5,OUTPUT);
+  pinMode(WaterSensor,OUTPUT);
+  Serial.begin(57600);
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+}
+void setup_wifi() {
 
+  delay(10);
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  randomSeed(micros());
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  client.subscribe("ESP8266/4");
+  client.subscribe("ESP8266/5");
+  client.subscribe("allow");
+}
+int WaterLevel()
+{
+  digitalWrite(WaterSensor,HIGH);
+  delay(10);
+  value=analogRead(sensorIn);
+  digitalWrite(WaterSensor,LOW);
+  return value;
 }
 
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  _topic="";
+  message="";
+  for (int i = 0; i < length; i++) {
+    message+=(char)payload[i];
+  }
+  message+="\n";
+  _topic+=topic;
+  _topic+="\n";
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ESP8266Client-";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (client.connect("ESP8266Client")) {
+      Serial.println("connected");
+      client.subscribe("ESP8266/4");
+      client.subscribe("ESP8266/5");
+      client.subscribe("allow");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");      
+    }
+  }
+}
+
+String splitString(String s,String deli)
+{
+ int from=s.indexOf(deli);
+ String result="";
+ String type="";
+ for(int i=0;i<from;i++)
+ {
+  type+=s[i];
+ }
+ for(int i=from+1;i<s.length();i++)
+ {
+  result+=s[i];
+ }
+ return result;
+  }
+
+ void read_data(const char* data)
+{
+   while(Serial.available()>0)
+  {
+    c=Serial.read();
+    if(c=='\n')
+    {
+      break;
+    }
+    else
+    {
+      dataIn+=c;
+    }
+  }
+  if(c=='\n')  
+  { 
+    dataIn.trim();
+    if(dataIn=="")
+    {
+     return;
+    }
+    else
+    {
+    client.publish(data,dataIn.c_str());
+    c=0;
+    dataIn="";
+    }
+  }
+}
 void loop() {
-String msg=uno.readStringUntil('\r');
-Serial.print(msg);
- delay(2000);
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+ if(counter<1)
+ {
+ client.publish(Toopic,mess_setup.c_str());
+ counter++;
+ }
+  String result=splitString(message,":");
+  result.trim();
+  int lenght=result.length();
+  char topic_sensor[lenght+1];
+  strcpy(topic_sensor,result.c_str());
+  unsigned long now = millis();
+  if (now - lastMsg > 1000) {
+      lastMsg=now;
+     read_data(topic_sensor);
+     result+="\n";
+     Serial.println(result);
+    String res=""+_topic+message;
+    
+  }
 }
